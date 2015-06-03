ae4985757c7dc807c02837c888f5b7b573c7c79f
Vishal Bardoloi <vishal@VishalspringMBP.fios-router.home>
2015-06-03 13:39:55 -0500

    Removed redundant comments

--- mopidy_qsaver/__init__.py
+++ mopidy_qsaver/__init__.py
@@ -4,9 +4,9 @@ import logging
 
 import os
 
-import gobject
+# import gobject
 
-import gst
+# import gst
 
 # import pygst
 
@@ -38,24 +38,6 @@ class Extension(ext.Extension):
         return schema
 
     def setup(self, registry):
-        # You will typically only implement one of the following things
-        # in a single extension.
 
-        # TODO: Edit or remove entirely
-        from .frontend import FoobarFrontend
-        registry.add('frontend', FoobarFrontend)
-
-        # TODO: Edit or remove entirely
-        from .backend import FoobarBackend
-        registry.add('backend', FoobarBackend)
-
-        # TODO: Edit or remove entirely
-        from .mixer import FoobarMixer
-        gobject.type_register(FoobarMixer)
-        gst.element_register(FoobarMixer, 'foobarmixer', gst.RANK_MARGINAL)
-
-        # TODO: Edit or remove entirely
-        registry.add('http:static', {
-            'name': self.ext_name,
-            'path': os.path.join(os.path.dirname(__file__), 'static'),
-        })
+        from .frontend import QSaverFrontend
+        registry.add('frontend', QSaverFrontend)
---
+++ mopidy_qsaver/qsaver.py
@@ -0,0 +1,26 @@
+import pykka 
+
+from mopidy.core import CoreListener
+
+class QSaverFrontend(pykka.ThreadingActor, CoreListener):
+	def __init__(self, config, core):
+		super(QSaverFrontend, self).__init__()
+        self.config = config
+        self.core = core
+
+    def saveQueue():
+    	# TODO: save the queue to a local file
+    	logger.info("Qsaver is saving the tracklist");
+
+    def restoreQueue():
+    	# TODO: restore the queue from a local file 
+    	logger.info("Qsaver is restoring your tracklist");
+
+    def on_start(self):
+        self.restoreQueue()
+
+    def on_stop(self):
+        self.saveQueue()
+
+    def track_playback_started(self, tl_track):
+    	self.saveQueue()
